7faf460240889c769ff58e5274627e222164ec83
Teng Han <hanteng1021@outlook.com>
2017-02-11 12:10:08 -0500

    tick test 5

--- ring_demo.py
+++ ring_demo.py
@@ -47,7 +47,6 @@ def tick_tick(serial_port):
         write_serial(serial_port, "e")
         tick_event = 1
         print("event 1 called")
-        """
     elif total_angle == 14 and tick_event == 1:
         write_serial(serial_port, "e")
         tick_event = 2
@@ -57,8 +56,7 @@ def tick_tick(serial_port):
         write_serial(serial_port, "i")
         tick_event = 0
         print("event 3 called")
-        """
-
+        
 
 
 
